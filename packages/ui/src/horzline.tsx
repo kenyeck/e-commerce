4c85fea0960f7daafa580bfb3547f6a20b74b086
Ken Yeck <967483+kenyeck@users.noreply.github.com>
2025-08-07 11:55:21 -0700

    Update horzline

--- packages/ui/src/horzline.tsx
+++ packages/ui/src/horzline.tsx
@@ -1,8 +1,17 @@
-import { HTMLProps } from "react";
+import { HTMLProps } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface HorizontalLineProps extends HTMLProps<HTMLHRElement> {}
 
-export function HorizontalLine({ id, className, ...styleProps }: HorizontalLineProps) {
-   return <hr id={id} className={className} {...styleProps} />;
+export function HorizontalLine({
+   id,
+   className = 'border-gray-300',
+   ...styleProps
+}: HorizontalLineProps) {
+   const lineStyleProps = className ? {} : { ...styleProps };
+   return (
+      <div className="py-2">
+         <hr id={id} className={className} {...lineStyleProps} />
+      </div>
+   );
 }
